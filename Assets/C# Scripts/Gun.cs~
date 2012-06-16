3582fa296cb5a6c9548db7f8757cb04f10994f58
Tetsunari Niina <konabanna@gmail.com>
2012-06-16 15:18:24 +0900

    作っている途中

---
+++ Assets/C# Scripts/Gun.cs~
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0.0f,-0.02f,1.0f);
+	}
+}
---
+++ Assets/C# Scripts/Missaile.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class Missaile : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//移動
+		transform.Translate(0.0f,-0.0f,2.0f);
+	}
+}
